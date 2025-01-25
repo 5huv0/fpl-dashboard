c479aba0514952fa7a0e574c3f76ce7c0975fedc
5huv0 <md88748874@gmail.com>
2025-01-26 01:10:50 +0600

    almost done navbar footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Home(){
     return(
-        <div>
-            
+        <div className="flex">
+            <div className="w-16 bg-slate-600">
+                div 1
+            </div>
+            <div className="w-64 flex-1 bg-green-500">
+                div 2
+            </div>
         </div>
     )
 }
