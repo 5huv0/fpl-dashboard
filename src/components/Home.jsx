b6ca77df94217826256d1cf02078d0d0d2f26e6b
5huv0 <md88748874@gmail.com>
2025-01-27 00:03:10 +0600

    working with design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,26 @@ import React from "react";
 export default function Home(){
     return(
         <div className="flex">
-            <div className="w-16 bg-slate-600">
-                div 1
+            <div className="flex1 bg-slate-700">
+            flex1
             </div>
-            <div className="w-64 flex-1 bg-green-500">
-                div 2
+            <div className="flex2 bg-gray-200">flex2
+                <div className="flex3 bg-green-600">flex3
+                    <div className="flex5 bg-red-600">flex5
+                        <div className="flex7 bg-blue-700">
+                            flex7
+                        </div>
+                        <div className="flex8 bg-orange-500">
+                            flex8
+                        </div>
+                    </div>
+                    <div className="flex6 bg-yellow-400">
+                        flex6
+                    </div>
+                </div>
+                <div className="flex4 bg-emerald-500">
+                    flex4
+                </div>
             </div>
         </div>
     )
